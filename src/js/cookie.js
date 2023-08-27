770f800f03d77e51f56e9a8932aec79dbde0b012
Eugene <nevin523348@gmail.com>
2023-08-27 13:20:21 +0300

    change css cookie

--- src/js/cookie.js
+++ src/js/cookie.js
@@ -1,20 +1,17 @@
 const customModal = document.getElementById("custom-modal");
-    const acceptCookiesBtn = document.getElementById("acceptCookies");
-    const dismissCookiesBtn = document.getElementById("dismissCookies");
-
-    // Check if cookies are accepted
-    const cookiesAccepted = localStorage.getItem("cookiesAccepted");
+const acceptCookiesBtn = document.getElementById("acceptCookies");
+const dismissCookiesBtn = document.getElementById("dismissCookies");
+const cookiesAccepted = localStorage.getItem("cookiesAccepted");
 
     if (!cookiesAccepted) {
-      customModal.style.display = "flex";
+        customModal.style.display = "flex";
     }
 
-    // Handle button clicks
     acceptCookiesBtn.addEventListener("click", () => {
-      customModal.style.display = "none";
-      localStorage.setItem("cookiesAccepted", "true");
+        customModal.style.display = "none";
+        localStorage.setItem("cookiesAccepted", "true");
     });
 
     dismissCookiesBtn.addEventListener("click", () => {
-      customModal.style.display = "none";
+        customModal.style.display = "none";
     });
